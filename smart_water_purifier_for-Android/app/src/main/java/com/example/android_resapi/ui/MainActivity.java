ed20555a020b87e9b9b01d10fa93803f71043167
yudini <oyj0922@naver.com>
2022-12-05 14:07:11 +0900

    UI 및 자바파일 수정

--- smart_water_purifier_for-Android/app/src/main/java/com/example/android_resapi/ui/MainActivity.java
+++ smart_water_purifier_for-Android/app/src/main/java/com/example/android_resapi/ui/MainActivity.java
@@ -21,24 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listThingsURL = findViewById(R.id.listThingsURL);
+      /*  listThingsURL = findViewById(R.id.listThingsURL);
         getShadowURL = findViewById(R.id.getShadowURL);
         updateShadowURL = findViewById(R.id.updateShadowURL);
         getLogsURL = findViewById(R.id.getLogsURL);
-
+*/
         //사물 목록 조회
         //listThingBtn 을 클릭시에 동작하는 listener
-        Button listThingsBtn = findViewById(R.id.listThingsBtn);
+        final Button listThingsBtn = findViewById(R.id.listThingsBtn);
         listThingsBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String urlstr = listThingsURL.getText().toString();
-                Log.i(TAG, "listThingsURL=" + urlstr);
-                if (urlstr == null || urlstr.equals("")) {   //url 입력이 없는경우
-                   Toast.makeText(MainActivity.this, "사물목록 조회 API URI 입력이 필요합니다.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                String listThingsURL = "https://kjgnr8bpzc.execute-api.ap-northeast-2.amazonaws.com/prod/devices";
+                Log.i(TAG, "listThingsURL=" + listThingsURL);
+                String listURL = "https://kjgnr8bpzc.execute-api.ap-northeast-2.amazonaws.com/prod/devices";
                 Intent intent = new Intent(MainActivity.this, ListThingsActivity.class);  // ListThingActivity화면으로 이동하기위한 인텐트
-                intent.putExtra("listThingsURL", listThingsURL.getText().toString());//listThingsURL을 같이 넘겨줌
+                intent.putExtra("listThingsURL",listURL);
                 startActivity(intent);  //새로운 화면으로 이동(사물 조회 화면)
                 //  new GetThings(MainActivity.this).execute();
                 //  new GetThingShadow(MainActivity.this, "MyMKRWiFi1010").execute();
@@ -52,13 +49,9 @@ public class MainActivity extends AppCompatActivity {
         getShadowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlstr = getShadowURL.getText().toString();
-                if (urlstr == null || urlstr.equals("")) {  //url 입력이 없는 경우
-                    Toast.makeText(MainActivity.this, "물그릇 상태 조회 API URI 입력이 필요합니다.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                String getShadowURL = "https://kjgnr8bpzc.execute-api.ap-northeast-2.amazonaws.com/prod/devices/Smart_Water_Purifier_for_Dog";
                 Intent intent = new Intent(MainActivity.this, DeviceActivity.class); //DeviceActivity화면으로 이동하기위한 인텐트
-                intent.putExtra("getShadowURL", getShadowURL.getText().toString());  //getShadowURL을 같이 넘겨줌
+                intent.putExtra("getShadowURL", getShadowURL);  //getShadowURL을 같이 넘겨줌
                 startActivity(intent);//새로운 화면으로 이동(물그릇 상태 조회 화면)
 
             }
@@ -70,13 +63,9 @@ public class MainActivity extends AppCompatActivity {
         updateShadowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlstr = updateShadowURL.getText().toString();
-                if (urlstr == null || urlstr.equals("")) {  //url 입력이 없는 경우
-                    Toast.makeText(MainActivity.this, "급수할 물 양 변경 API URI 입력이 필요합니다.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                String updateShadowURL = "https://kjgnr8bpzc.execute-api.ap-northeast-2.amazonaws.com/prod/devices/Smart_Water_Purifier_for_Dog";
                 Intent intent = new Intent(MainActivity.this, UpdateActivity.class); //UpdateActivity화면으로 이동하기위한 인텐트
-                intent.putExtra("updateShadowURL", updateShadowURL.getText().toString());//updateShadowURL을 같이 넘겨줌
+                intent.putExtra("updateShadowURL", updateShadowURL);//updateShadowURL을 같이 넘겨줌
                 startActivity(intent); //새로운 화면으로 이동(물그릇 상태 조회 화면)
 
             }
@@ -88,13 +77,9 @@ public class MainActivity extends AppCompatActivity {
         listLogsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlstr = getLogsURL.getText().toString();
-                if (urlstr == null || urlstr.equals("")) {  //url 입력이 없는 경우
-                    Toast.makeText(MainActivity.this, "물 양 조회 API URI 입력이 필요합니다.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                String getLogsURL = "https://kjgnr8bpzc.execute-api.ap-northeast-2.amazonaws.com/prod/devices/WaterLevel/log";
                 Intent intent = new Intent(MainActivity.this, LogActivity.class);//LogActivity화면으로 이동하기위한 인텐트
-                intent.putExtra("getLogsURL", getLogsURL.getText().toString());//getLogURL을 같이 넘겨줌
+                intent.putExtra("getLogsURL", getLogsURL);//getLogURL을 같이 넘겨줌
                 startActivity(intent);
             }
         });
